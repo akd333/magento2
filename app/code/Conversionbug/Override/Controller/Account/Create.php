51732dadc89dd1b969451158090b45b93a0adecb
Ashish <ashish.dhar@conversionbug.com>
2018-07-21 11:45:56 +0530

    Override test

--- app/code/Conversionbug/Override/Controller/Account/Create.php
+++ app/code/Conversionbug/Override/Controller/Account/Create.php
@@ -13,10 +13,55 @@ use Magento\Framework\App\Action\Context;
 
 class Create extends \Magento\Customer\Controller\AbstractAccount
 {
+    /**
+     * @var \Magento\Customer\Model\Registration
+     */
+    protected $registration;
+
+    /**
+     * @var Session
+     */
+    protected $session;
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param Session $customerSession
+     * @param PageFactory $resultPageFactory
+     * @param Registration $registration
+     */
+    public function __construct(
+        Context $context,
+        Session $customerSession,
+        PageFactory $resultPageFactory,
+        Registration $registration
+    ) {
+        $this->session = $customerSession;
+        $this->resultPageFactory = $resultPageFactory;
+        $this->registration = $registration;
+        parent::__construct($context);
+    }
+
+    /**
+     * Customer register form page
+     *
+     * @return \Magento\Framework\Controller\Result\Redirect|\Magento\Framework\View\Result\Page
+     */
     public function execute()
     {
-        echo "Controller overridden";
-        //return parent::execute();
+        if ($this->session->isLoggedIn() || !$this->registration->isAllowed()) {
+            /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
+            $resultRedirect = $this->resultRedirectFactory->create();
+            $resultRedirect->setPath('*/*');
+            return $resultRedirect;
+        }
 
+        /** @var \Magento\Framework\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
     }
 }
